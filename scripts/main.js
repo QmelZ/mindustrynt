ad5741911dfc0d80098ff48186f9f2710529bfcc
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-01-04 05:27:00 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 Events.on(ClientLoadEvent, () => {
-    Timer.schedule(() => {
+    Events.on(Trigger.update.class, () => {
         Vars.ui.menuGroup.children.get(0).children.get(1).clearChildren();
-    }, 2, 0.2, 15);
+    });
     var table = new Table(Tex.clear);
     table.margin(12);
     table.button("", new TextureRegionDrawable(Core.atlas.find("clear")), Styles.cleart, () => {
