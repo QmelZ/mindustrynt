d7b17f262e524aa0ba2d128afb609c6627555075
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-05-23 18:08:01 +0300

    idc lmao i'll fix it

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ Events.on(ClientLoadEvent, () => {
     Events.on(Trigger.update.class, () => {
         Vars.ui.menuGroup.children.get(0).children.get(1).clearChildren();
     });
-    var table = new Table(Tex.clear);
+    let table = new Table(Tex.clear);
     table.margin(12);
     table.button("", new TextureRegionDrawable(Core.atlas.find("clear")), Styles.cleart, () => {
         Core.settings.put("mod-mindustrynt-enabled", false);
